44859cea41aa7757428b7bef967d3d9b36dcdd31
kkdlau <danny1251597530@gmail.com>
2020-12-26 23:08:01 +0800

    Complete recording button animation

--- lib/video_recording_page/recording_button.dart
+++ lib/video_recording_page/recording_button.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/material.dart';
 
-class RecordingButton extends AnimatedWidget {
-  final Widget child;
-  final shapeTween;
-  final bool isRecording;
+class RecordingButton extends StatelessWidget {
   final void Function() onPressed;
-  const RecordingButton(
-      {Key key,
-      this.child,
-      Animation<double> animation,
-      this.shapeTween,
-      this.isRecording,
-      this.onPressed})
-      : super(key: key, listenable: animation);
+  final bool isRecording;
+  const RecordingButton({Key key, this.onPressed, this.isRecording})
+      : super(key: key);
+
+  static const double BUTTON_SIZE = 70.0;
 
   @override
   Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
-    return Container(
-      padding: EdgeInsets.all(3.0),
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(color: Colors.white, width: 5.0)),
-      child: Container(
-        width: 50.0,
-        height: 50.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.red,
+    return GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          width: BUTTON_SIZE,
+          height: BUTTON_SIZE,
+          padding: EdgeInsets.all(3.0),
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.white, width: 5.0)),
+          child: InnerButton(isRecording: isRecording),
+        ));
+  }
+}
+
+class InnerButton extends StatelessWidget {
+  const InnerButton({
+    Key key,
+    @required this.isRecording,
+  }) : super(key: key);
+
+  @required
+  final bool isRecording;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: UnconstrainedBox(
+        child: AnimatedContainer(
+          width: isRecording ? 25.0 : 50.0,
+          height: isRecording ? 25.0 : 50.0,
+          duration: const Duration(milliseconds: 100),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(isRecording ? 5.0 : 25.0),
+            color: Colors.red,
+          ),
         ),
       ),
     );
